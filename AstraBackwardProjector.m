750ea9498ff81895aa09a6068c826a42794a4be0
maria120123 <s171246@student.dtu.dk>
2023-04-23 12:08:26 +0200

    Multiplications

--- AstraBackwardProjector.m
+++ AstraBackwardProjector.m
@@ -11,12 +11,37 @@ classdef AstraBackwardProjector < AstraProjector
 
         end
 
-        % Matrix multiplication
-        function y = mtimes(B, x)
+        % Matrix multiplication B*b
+        function y = mtimes(b,vol_geom,proj_geom,num_angles,num_dets,proj_id)
+            % Inputs:
+            %   b:
+            %   vol_geom: 
+            %   proj_geom: Projection geometry 'parallel' or 'fanflat'
+            %   num_angles: Number of view angles
+            %   num_dets:   Number of detector elements
+            %   proj_id:
+            %
+            % Output:
+            %   y: Back projection B*b
+
+            
             % Size check
 
             % Call ASTRA
             % --- INSERT CODE HERE ---
+            sinogram_id = astra_mex_data2d('create', '-sino', proj_geom, reshape(b,num_angles,num_dets));
+            recon_id = astra_mex_data2d('create', '-vol', vol_geom, 0);
+
+            cfg = astra_struct('BP_CUDA');
+            cfg.ProjectorId = proj_id;
+            cfg.ProjectionDataId = sinogram_id;
+            cfg.ReconstructionDataId = recon_id;
+
+            bp_id = astra_mex_algorithm('create', cfg);
+            astra_mex_algorithm('run', bp_id);
+            Bb = astra_mex_data2d('get', recon_id);
+            y = Bb(:);
+
         end
     end
 
--- AstraForwardProjector.m
+++ AstraForwardProjector.m
@@ -12,13 +12,40 @@ classdef AstraForwardProjector < AstraProjector
 
         end
 
-        % Matrix multiplication
-        function y = mtimes(A, x)
-            % Size check
+        % Matrix multiplication A*x
+        function y = mtimes(x,vol_geom,proj_geom,num_pixels,proj_id)
+            % Inputs:
+            %   x:
+            %   vol_geom: 
+            %   proj_geom: Projection geometry 'parallel' or 'fanflat'
+            %   num_pixels: Number of pixels in a row/coloumn
+            %   proj_id:    
+            %
+            % Output:
+            %   y: Forward projection A*x
+
             
+            % Size check
+
 
             % Call ASTRA
             % --- INSERT CODE HERE ---
+            volume_id = astra_mex_data2d('create', '-vol', vol_geom, reshape(x,num_pixels,num_pixels));
+            sinogram_id = astra_mex_data2d('create', '-sino', proj_geom, 0);
+
+            cfg = astra_struct('FP_CUDA');
+
+            cfg.ProjectorId = proj_id;            % Id to forward projector A
+            cfg.ProjectionDataId = sinogram_id;   % Id to sinogram b (what A should be multiplied with???)
+            cfg.VolumeDataId = volume_id;         % Id to size/volume of output
+
+            fp_id = astra_mex_algorithm('create', cfg);
+            astra_mex_algorithm('run', fp_id);
+            sinogram = astra_mex_data2d('get', sinogram_id);
+            y = sinogram(:);
+
+            astra_mex_data2d('delete', sinogram_id, volume_id);
+            astra_mex_algorithm('delete', fp_id);
         end
 
     end
