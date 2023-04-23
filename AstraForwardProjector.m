0da3d31af0762a9e72144fe4b6d5f4558e138c35
mhg <mhg@ticra.com>
2023-04-23 11:08:48 +0200

    Added matvec routines for A and B

--- AstraForwardProjector.m
+++ AstraForwardProjector.m
@@ -1,5 +1,16 @@
 classdef AstraForwardProjector < AstraProjector
 
+    methods
 
+        % Matrix multiplication
+        function y = mtimes(A, x)
+            % Size check
+            
+
+            % Call ASTRA
+            % --- INSERT CODE HERE ---
+        end
+
+    end
 
 end
