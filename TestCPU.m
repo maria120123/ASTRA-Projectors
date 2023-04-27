ae3fce206f720ca67e52f0a26e2761b0db341da5
maria120123 <s171246@student.dtu.dk>
2023-04-27 20:54:21 +0200

    Update TestCPU.m

--- TestCPU.m
+++ TestCPU.m
@@ -8,22 +8,44 @@ addpath("~/Desktop/ABBA_projectors/ASTRA-Projectors") % Path to toolbox
 astra_setup("~/astra") % Only needs to be run once
 
 %% Set up CT specifications and create the projector pair
-num_pixels = 32;
+num_pixels = 64;
 num_angles = 180;
-num_detectors = 32;
+num_detectors = 64;
 det_width = 1;
+GPU = 0;
 
-[A, B] = astra_projectors(0, num_pixels, num_angles, num_detectors, ...
+[A, B] = astra_projectors(GPU, num_pixels, num_angles, num_detectors, ...
     det_width);
 
-%% Create full matrix
-Af = sparse(A);
+%% Size of operators
+A_size = size(A);
+B_size = size(B);
 
-%%
-Bf = sparse(B);
+%% Create sparse forward projector matrix
+% Note: ASTRA provide the forward operator matrix
+Af = sparse(A); % full(A) is also availabe
 
-%% Plot one ray
+%% Create sparse backward projector matrix
+Bf = sparse(B); % full(B) is also availabe
+
+%% Plot one ray moving through the image x
 idx = 313;
 xray = reshape(Af(idx, :), num_pixels, num_pixels);
 
 imagesc(xray)
+
+%% Solve CT problem
+X = phantom('Modified Shepp-Logan',num_pixels);
+x = X(:);
+
+sinogram = reshape(A*x,num_angles,num_detectors);
+
+figure();
+imagesc(sinogram)
+title('Sinogram')
+
+recon = reshape(B*sinogram(:),num_pixels,num_pixels);
+
+figure();
+imagesc(recon)
+title('Reconstruction')
